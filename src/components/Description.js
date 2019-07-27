ecd2a371d403d638a3cb5ee442778f52f1621742
ironman <insideminder@yahoo.com>
2019-07-27 17:47:46 +0300

    Responsiveness update

--- src/components/Description.js
+++ src/components/Description.js
@@ -8,10 +8,10 @@ const Description =()=>{
   const {addToCart} = Utility();
            return (
                         <div className="row d-flex align-items-center justify-content-center mx-auto">
-                             <div className="col-9 col-lg-6">
+                             <div className="col-9 col-lg-6 mb-5">
                                 <img className="img-fluid bg-white" alt={title} src={img}/>
                              </div>
-                            <div className="col-9 col-lg-6">
+                            <div className="col-9 col-lg-6 mb-3">
                                 <p className="text-lead">{info}</p>
                                   {
                                      inCart? /*IF*/
